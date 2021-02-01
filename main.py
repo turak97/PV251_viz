f48a1cbed926ff2f769c2f59b0c9fc07393bf622
xjoch1 <turak97@gmail.com>
2021-02-01 18:07:10 +0100

    heroku try

--- main.py
+++ main.py
@@ -2,15 +2,12 @@
 import argparse  # to easily parse arguments
 import sys  # sys.argv[1:] -- to obtain possible file path
 
-from bokeh.server.server import Server
-from bokeh.core import validation
+from bokeh.plotting import curdoc
 
 import layout_factory as lf
 
 import pandas as pd
 
-
-validation.silence(1002, True)  # silence the bokeh plot warnings
 
 
 def hardwired_data():
@@ -50,36 +47,21 @@ def parse_args():
 
 
 if __name__ == '__main__':
-
-    args = parse_args()
-    path = args[0]
-    x_name, y_name = args[1]
+    #
+    # args = parse_args()
+    # path = args[0]
+    # x_name, y_name = args[1]
+    path = None
+    x_name = 'Year, 1956 = 0'
+    y_name = 'Birds (sqrt)'
 
     data_frame = None
     if path == None:
         data_frame = hardwired_data()
-        x_name = 'Year, 1956 = 0'
-        y_name = 'Birds (sqrt)'
     else:
         data_frame = pd.read_csv(path)
         data_frame = data_frame[[x_name, y_name]].copy()
-    #
-    # ##### DEL AFTER; PREPROCESSING
-    # data_frame[y_name] = data_frame[y_name].apply(lambda x: np.sqrt(x))
-    # data_frame[x_name] = data_frame[x_name].apply(lambda x: x - min(data_frame[x_name]))
 
-    def bkapp(doc):
+    layout = lf.Layout(data_frame, x_name, y_name)
 
-        layout = lf.Layout(data_frame, x_name, y_name)
-
-        doc.add_root(layout.layout)
-
-    server = Server({'/': bkapp}, num_procs=1)
-    server.start()
-
-    server.io_loop.add_callback(server.show, "/")
-    try:
-        server.io_loop.start()
-    except KeyboardInterrupt:
-        print()
-        print("Session closed, have a nice day.")
+    curdoc().add_root(layout.layout)
